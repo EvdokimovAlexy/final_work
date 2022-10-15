b6b3177b9e91fc988fe4fcb8984a2d4cc4280778
Prometheus <poligraphich@gmail.com>
2022-10-15 20:48:50 +0300

    change prog

--- final_work/Program.cs
+++ final_work/Program.cs
@@ -1,8 +1,8 @@
 ﻿string[] array1 = new string[5] { "2", "world", "hello", "Russia", ":-)" };
 string[] array2 = new string[array1.Length];
-SecondArrayWithIF(array1, array2);
+SecondArray(array1, array2);
 PrintArray(array2);
-void SecondArrayWithIF(string[] array1, string[] array2)
+void SecondArray(string[] array1, string[] array2)
 {
     int count = 0;
     for (int i = 0; i < array1.Length; i++)
